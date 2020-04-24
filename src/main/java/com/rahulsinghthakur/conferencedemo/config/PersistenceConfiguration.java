522a1abf390b13bee080560a6eddee49ab707629
rahulsinghthakur <rahulsingh12353@gmail.com>
2020-04-23 17:21:24 -0700

    Initial Commit: REST API VERSION 1.0.2

--- src/main/java/com/rahulsinghthakur/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/rahulsinghthakur/conferencedemo/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.println("datasource bean has been initiated and set");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.println("datasource bean has been initiated and set");
+//        return builder.build();
+//    }
 }
